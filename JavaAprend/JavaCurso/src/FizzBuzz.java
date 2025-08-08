3e711064d252ab0482351305ef78f7d6c2f20a52
plamin <ph856776@gmail.com>
2025-08-08 12:54:38 -0300

    feat: add FizzBuzz

--- JavaAprend/JavaCurso/src/FizzBuzz.java
+++ JavaAprend/JavaCurso/src/FizzBuzz.java
@@ -3,13 +3,16 @@ public class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while (i <= 100){
-            if (i % 3 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                i++;
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
                 i++;
-            } else if (i % 5 == 0) {
+            } else if(i % 5 == 0){
                 System.out.println("Buzz");
                 i++;
-            } else {
+            }else {
                 System.out.println(i);
                 i++;
             }
