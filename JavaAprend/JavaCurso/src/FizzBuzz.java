ffe36f148bc7f88c9563c8327cc72a0bb06bf0d0
plamin <ph856776@gmail.com>
2025-08-08 12:51:46 -0300

    FizzBuzz

---
+++ JavaAprend/JavaCurso/src/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100){
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+                i++;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                i++;
+            } else {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+}
